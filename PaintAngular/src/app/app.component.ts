ed2a2ff4af36622401d9df46c0c9cd9090ec2177
A.Wael <96313471+AbdelrahmanWael2@users.noreply.github.com>
2022-12-02 18:15:25 +0200

    handles json requests

---
+++ PaintAngular/src/app/app.component.ts
@@ -0,0 +1,115 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+//array of objects to be drawn
+const allShapes: any[] = []
+
+export interface IShape
+{
+  x:number
+  y:number
+  xP:number
+  yP:number
+  name:string
+}
+
+  @Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']})
+
+export class AppComponent {
+
+
+ 
+
+ constructor(private http:HttpClient){}
+  
+  title = 'Paint';
+  ///some flags////////////////
+  rectangle = false
+  X:any
+  Y:any
+  x:any
+  y:any
+  // var Shape : IShape|null;
+  undo = false
+//////////////////////////////
+  @ViewChild('canvas', {static: true }) mycanvas!: ElementRef;
+  ngOnInit(): void{
+    const canvas: HTMLCanvasElement = this.mycanvas.nativeElement;
+    const context = canvas.getContext('2d');
+  }
+  
+   //gets desired shape from back 
+   create()
+   {
+    const canvas: HTMLCanvasElement = this.mycanvas.nativeElement
+    const context = canvas.getContext('2d');
+    //rectangle button pressed
+    if(this.rectangle == true)
+    {  var Shape : IShape
+      this.http.post<Object>("http://localhost:9090/createShape", "rectangle")
+      .subscribe((res:any)=> Shape = res)
+      
+      
+      //Captures the position clicked by the user to set the new X and Y
+      canvas.addEventListener("click", e=>{
+      if(this.rectangle)
+      {
+      this.X = parseInt(e.offsetX.toString());
+      this.Y = parseInt(e.offsetY.toString());
+      Shape.xP = this.X
+      Shape.yP = this.Y
+      
+      console.log(Shape)
+
+      //sending the new rectangle to be stored in back
+      this.http.post<IShape>("http://localhost:9090/store",
+      {
+        x:Shape.x,
+        y:Shape.y,
+        yP:Shape.yP,
+        xP:Shape.xP,
+        name:Shape.name
+      }).subscribe((data : IShape) => {Shape = data
+      ,console.log(allShapes.length),
+      console.log(allShapes)
+    } )
+      allShapes.push(Shape)
+      this.draw()
+      
+      
+      console.log(Shape.xP)
+      
+      }})}}
+  //draws given shapes
+  draw()
+  {
+    const canvas: HTMLCanvasElement = this.mycanvas.nativeElement
+    const context = canvas.getContext('2d');
+    //console.log(allShapes)
+    for(let i = 0 ; i < allShapes.length; i++)
+    { 
+       if(context && allShapes[i].name === "rectangle" )
+       {
+        context.strokeRect(allShapes[i].xP,allShapes[i].yP,allShapes[i].x,allShapes[i].y)
+        this.rectangle = false
+       }
+    }
+    
+  }
+  Undo()
+  { const canvas: HTMLCanvasElement = this.mycanvas.nativeElement
+    const context = canvas.getContext('2d');
+    //clears canvas
+    if(context)
+    {context.clearRect(0, 0, canvas.width, canvas.height);}
+    //pops last shape
+    if(this.undo)
+    {allShapes.pop()}
+    this.undo = false
+    //redraw the new canvas
+    this.draw()
+  }
+}
---
+++ PaintAngular/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [
+    HttpClient,
+    HttpClientModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
