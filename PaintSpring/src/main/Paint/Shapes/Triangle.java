0e0b5b39f7c3b9466616f38ea939fea619ad8d4a
abdelrahman mohamed mohamed abdelmotlb <91913879+abdelmotlb@users.noreply.github.com>
2022-12-07 19:58:33 +0200

    before copy DP

---
+++ PaintSpring/src/main/Paint/Shapes/Triangle.java
@@ -0,0 +1,138 @@
+package com.Paint.Paint.Shapes;
+
+public class Triangle {
+    
+    private String Name = "triangle";
+    private String id = "";
+
+    private double xP = 0;
+    private double yP = 0;
+
+    private double x = 70;
+    private double y = 70;
+
+    private String border = "black";
+
+    private double borderWidth = 5;
+    private boolean borderScaleEnabled = false;
+
+    private String fill = "white";
+    private boolean fillEnabled = false;
+    private double rotation = 0;
+
+    private double points[] = {0,0,0,100,100,100};
+
+    public Triangle(){}
+
+    public Triangle(String id, String Name, int xP, int yP, int x, int y, String border, int borderWidth, boolean borderScaleEnabled, String fill, boolean fillEnabled, int rotation, double points[]) {
+        this.id = id;
+        this.Name = Name;
+        this.xP = xP;
+        this.yP = yP;
+        this.x = x;
+        this.y = y;
+        this.border = border;
+        this.borderWidth = borderWidth;
+        this.borderScaleEnabled = borderScaleEnabled;
+        this.fill = fill;
+        this.fillEnabled = fillEnabled;
+        this.rotation = rotation;
+        this.points = points;
+    }
+
+
+    // start setters and getters
+
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getxP() {
+        return xP;
+    }
+    public void setxP(double xP) {
+        this.xP = xP;
+    }
+
+    public double getyP() {
+        return yP;
+    }
+    public void setyP(double yP) {
+        this.yP = yP;
+    }
+
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getBorder() {
+        return border;
+    }
+    public void setBorder(String border) {
+        this.border = border;
+    }
+
+    public double getBorderWidth() {
+        return borderWidth;
+    }
+    public void setBorderWidth(double borderWidth) {
+        this.borderWidth = borderWidth;
+    }
+
+    public boolean isBorderScaleEnabled() {
+        return borderScaleEnabled;
+    }
+    public void setBorderScaleEnabled(boolean borderScaleEnabled) {
+        this.borderScaleEnabled = borderScaleEnabled;
+    }
+
+    public String getFill() {
+        return fill;
+    }
+    public void setFill(String fill) {
+        this.fill = fill;
+    }
+
+    public boolean isFillEnabled() {
+        return fillEnabled;
+    }
+    public void setFillEnabled(boolean fillEnabled) {
+        this.fillEnabled = fillEnabled;
+    }
+
+    public double getRotation() {
+        return rotation;
+    }
+    public void setRotation(double rotation) {
+        this.rotation = rotation;
+    }
+
+    public double[] getPoints() {
+        return points;
+    }
+    public void setPoints(double[] points) {
+        this.points = points;
+    }
+
+    // end setters and getters.
+}
