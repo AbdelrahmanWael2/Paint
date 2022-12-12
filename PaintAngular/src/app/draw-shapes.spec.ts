5ed69b19c8fb1229180c6c10df3e5ba1aeee7e89
Ahmed Hesham <ahmedhesham.1652001@gmail.com>
2022-12-12 19:42:26 +0200

    Delete PaintAngular/src/app directory

--- PaintAngular/src/app/draw-shapes.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DrawShapes } from './draw-shapes';
-
-describe('DrawShapes', () => {
-  it('should create an instance', () => {
-    expect(new DrawShapes()).toBeTruthy();
-  });
-});
--- PaintAngular/src/app/shape-attr.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ShapeAttr } from './shape-attr';
-
-describe('ShapeAttr', () => {
-  it('should create an instance', () => {
-    expect(new ShapeAttr()).toBeTruthy();
-  });
-});
